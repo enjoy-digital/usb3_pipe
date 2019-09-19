afdebbcd3c922551058fcf63018906ca7bf6b2e6
Florent Kermarrec <florent@enjoy-digital.fr>
2019-09-19 18:57:02 +0200

    pocs: add lfps poc with capture of first lfps after connecting to the host
    tBurst: 1us, tRepeat=10us: Polling LFPS

---
+++ pocs/lfps/test_analyzer.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+
+from litex import RemoteClient
+from litescope import LiteScopeAnalyzerDriver
+
+wb = RemoteClient()
+wb.open()
+
+# # #
+
+# FPGA ID ------------------------------------------------------------------------------------------
+fpga_id = ""
+for i in range(256):
+    c = chr(wb.read(wb.bases.identifier_mem + 4*i) & 0xff)
+    fpga_id += c
+    if c == "\0":
+        break
+print("FPGA: " + fpga_id)
+
+# Enable Capture -----------------------------------------------------------------------------------
+wb.regs.gtp_rx_polarity.write(0)
+wb.regs.gtp_tx_enable.write(1)
+while (wb.regs.gtp_tx_ready.read() == 0):
+	pass
+wb.regs.gtp_rx_enable.write(1)
+while (wb.regs.gtp_rx_ready.read() == 0):
+	pass
+
+# Analyzer dump ------------------------------------------------------------------------------------
+analyzer = LiteScopeAnalyzerDriver(wb.regs, "analyzer", debug=True)
+analyzer.configure_subsampler(1)
+analyzer.configure_trigger(cond={"rxelecidle": 0})
+analyzer.run(offset=32, length=4096)
+analyzer.wait_done()
+analyzer.upload()
+analyzer.save("analyzer.vcd")
+
+# # #
+
+wb.close()
