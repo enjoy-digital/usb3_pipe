fdae840ad4b62407e9390fa3887a7b19332224f5
Florent Kermarrec <florent@enjoy-digital.fr>
2019-09-20 08:35:40 +0200

    create usb3_pipe directory, move symbols to it,  add setup

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+from setuptools import setup
+from setuptools import find_packages
+
+
+if sys.version_info[:3] < (3, 5):
+    raise SystemExit("You need Python 3.5+")
+
+
+setup(
+    name="ubs3_pipe",
+    version="0.1.dev",
+    description="Experiments with USB3.0 PIPE interface and FPGAs",
+    long_description=open("README").read(),
+    author="Florent Kermarrec",
+    author_email="florent@enjoy-digital.fr",
+    url="http://enjoy-digital.fr",
+    download_url="https://github.com/enjoy-digital/usb3_pipe",
+    test_suite="test",
+    license="BSD",
+    platforms=["Any"],
+    keywords="HDL ASIC FPGA hardware design",
+    classifiers=[
+        "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
+        "Environment :: Console",
+        "Development Status :: Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    packages=find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
+    include_package_data=True,
+)
