1405ee352564c5e349aa3dfda69bf15d77f57ba0
Florent Kermarrec <florent@enjoy-digital.fr>
2019-10-29 20:55:17 +0100

    core: remove enable signal to simplify, CEInserter or ResetInserter could be used on USB3PIPE is needed

--- usb3_pipe/core.py
+++ usb3_pipe/core.py
@@ -15,8 +15,7 @@ from usb3_pipe.scrambling import Scrambler, Descrambler
 class USB3PIPE(Module):
     def __init__(self, serdes, sys_clk_freq, with_scrambling=True):
         assert sys_clk_freq > 125e6
-        self.enable = Signal(reset=1) # i
-        self.ready  = Signal()        # o
+        self.ready  = Signal() # o
 
         self.sink   = stream.Endpoint([("data", 32), ("ctrl", 4)])
         self.source = stream.Endpoint([("data", 32), ("ctrl", 4)])
@@ -25,28 +24,22 @@ class USB3PIPE(Module):
 
         # LFPS -------------------------------------------------------------------------------------
         lfps = LFPSUnit(sys_clk_freq=sys_clk_freq, serdes=serdes)
-        lfps = ResetInserter()(lfps)
-        self.comb += lfps.reset.eq(~self.enable)
         self.submodules.lfps = lfps
 
         # TS----------------------------------------------------------------------------------------
         ts = TSUnit(serdes=serdes)
-        ts = ResetInserter()(ts)
-        self.comb += ts.reset.eq(~self.enable)
         self.submodules.ts = ts
 
         # LTSSM ------------------------------------------------------------------------------------
         ltssm = LTSSM(serdes=serdes, lfps_unit=lfps, ts_unit=ts, sys_clk_freq=sys_clk_freq)
-        ltssm = ResetInserter()(ltssm)
-        self.comb += ltssm.reset.eq(~self.enable)
         self.submodules.ltssm = ltssm
         self.comb += self.ready.eq(ltssm.polling.idle)
 
         # Scrambling -------------------------------------------------------------------------------
         if with_scrambling:
             scrambler = Scrambler()
-            scrambler = ResetInserter()(scrambler)
-            self.comb += scrambler.reset.eq(~self.ready)
+            scrambler = CEInserter()(scrambler)
+            self.comb += scrambler.ce.eq(self.ready)
             self.submodules.scrambler = scrambler
             self.comb += [
                 self.sink.connect(scrambler.sink),
@@ -54,8 +47,8 @@ class USB3PIPE(Module):
             ]
 
             descrambler = Descrambler()
-            descrambler = ResetInserter()(descrambler)
-            self.comb += descrambler.reset.eq(~self.ready)
+            descrambler = CEInserter()(descrambler)
+            self.comb += descrambler.ce.eq(self.ready)
             self.submodules.descrambler = descrambler
             self.comb += [
                 If(self.ready, serdes.source.connect(descrambler.sink)),
