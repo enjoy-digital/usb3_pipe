842d5c6aa4a81ff2e4150d0858e87eaacfe12594
Florent Kermarrec <florent@enjoy-digital.fr>
2019-10-31 19:32:10 +0100

    test/test_training: remove test_tseq_checker

--- test/test_training.py
+++ test/test_training.py
@@ -10,39 +10,6 @@ from usb3_pipe.training import TSChecker, TSGenerator
 
 
 class TestTraining(unittest.TestCase):
-    def test_tseq_checker(self):
-        tseq_length = len(TSEQ.to_bytes())//4
-        tseq_words  = [int.from_bytes(TSEQ.to_bytes()[4*i:4*(i+1)], "little") for i in range(tseq_length)]
-        def generator(dut, n_loops):
-            for i in range(n_loops):
-                for j in range(tseq_length):
-                    yield dut.sink.valid.eq(1)
-                    yield dut.sink.ctrl.eq(j == 0)
-                    yield dut.sink.data.eq(tseq_words[j])
-                    yield
-                    yield dut.sink.valid.eq(0)
-                    yield
-            for i in range(1024):
-                yield
-            dut.run = False
-
-        def checker(dut, n_loops, n_ordered_sets):
-            count = 0
-            while dut.run:
-                if (yield dut.detected):
-                    count += 1
-                yield
-            self.assertEqual(count, n_loops/n_ordered_sets)
-
-        dut = TSChecker(ordered_set=TSEQ, n_ordered_sets=4)
-        dut.run = True
-        generators = [
-            generator(dut, n_loops=32),
-            checker(dut, n_loops=32, n_ordered_sets=4),
-        ]
-        run_simulation(dut, generators)
-
-
     def test_ts1_checker(self):
         ts1_length = len(TS1.to_bytes())//4
         ts1_words  = [int.from_bytes(TS1.to_bytes()[4*i:4*(i+1)], "little") for i in range(ts1_length)]
