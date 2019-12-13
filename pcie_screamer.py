6e12c8f0f3067806c9321acdd090cd65cb0d9057
Florent Kermarrec <florent@enjoy-digital.fr>
2019-12-13 08:23:50 +0100

    pcie_screamer: reduce sys_clk_freq to 125MHz (synchronous with the 250MHz tx clk since generated from the same clk source)

--- pcie_screamer.py
+++ pcie_screamer.py
@@ -87,7 +87,7 @@ class _CRG(Module):
 class USB3SoC(SoCMini):
     def __init__(self, platform, with_analyzer=False):
 
-        sys_clk_freq = int(150e6)
+        sys_clk_freq = int(125e6)
         SoCMini.__init__(self, platform, sys_clk_freq, ident="USB3SoC", ident_version=True)
 
         # CRG --------------------------------------------------------------------------------------
